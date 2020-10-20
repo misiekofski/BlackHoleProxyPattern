551e8d52da65da74f08c2eb67dacba7a66e38385
Michal Dobrzycki <michal.dobrzycki@kincentric.com>
2020-10-20 12:07:59 +0200

    Working cutoff for images

--- BlackHoleProxyPattern/SeleniumBlackHoleProxyTest.cs
+++ BlackHoleProxyPattern/SeleniumBlackHoleProxyTest.cs
@@ -1,21 +1,24 @@
 using System;
+using System.Threading;
 using BlackHoleProxyPattern.Pages;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools;
 using OpenQA.Selenium.DevTools.Network;
 
 namespace BlackHoleProxyPattern
 {
     public class SeleniumBlackHoleProxyTest
     {
-        private ChromeDriver _driver;
+        private IWebDriver driver;
 
         [SetUp]
         public void Setup()
         { 
-            _driver = new ChromeDriver();
-            var session = _driver.CreateDevToolsSession();
+            driver = new ChromeDriver();
+            IDevTools devTools = driver as IDevTools;
+            var session = devTools.CreateDevToolsSession();
             
             var blockedUrlSettings = new SetBlockedURLsCommandSettings();
             blockedUrlSettings.Urls = new string[]
@@ -23,33 +26,35 @@ namespace BlackHoleProxyPattern
                 "http://automationpractice.com/img/p/1/6/16-large_default.jpg", 
                 "http://automationpractice.com/modules/blockbanner/img/sale70.png",
             };
-            
+
+            session.Network.Enable(new EnableCommandSettings());
             session.Network.SetBlockedURLs(blockedUrlSettings);
-            _driver.Manage().Window.Maximize();
-            _driver.Navigate().GoToUrl("http://automationpractice.com/");
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://automationpractice.com/");
+            Thread.Sleep(10000);
         }
 
         [Test]
         public void TestFunctionalityWithBlockedImages()
         {
-            var homePage = new HomePage(_driver);
+            var homePage = new HomePage(driver);
             homePage.SearchProduct("Yellow");
 
-            var resultsPage = new ResultsPage(_driver);
+            var resultsPage = new ResultsPage(driver);
             resultsPage.SelectNthProduct(2);
 
-            var productPage = new ProductPage(_driver);
+            var productPage = new ProductPage(driver);
             productPage.AddProductToCart();
             productPage.ProceedToCheckout();
 
-            Screenshot ss = ((ITakesScreenshot)_driver).GetScreenshot();
+            Screenshot ss = ((ITakesScreenshot)driver).GetScreenshot();
             ss.SaveAsFile("screenshot.png", ScreenshotImageFormat.Png);
         }
 
         [TearDown]
         public void TearDown()
         {
-            _driver.Quit();
+            driver.Quit();
         }
     }
 }
